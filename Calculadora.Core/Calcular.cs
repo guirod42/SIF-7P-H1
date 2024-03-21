b6096d4df6d94752a36725299c3fff361f7ae7df
Guilherme Silva Rodrigues <69553240+guirod42@users.noreply.github.com>
2024-03-21 20:59:21 -0300

    Update Calcular.cs

--- Calculadora.Core/Calcular.cs
+++ Calculadora.Core/Calcular.cs
@@ -8,7 +8,7 @@
 
         public static int Dividir(int x, int y)
         {
-            if (y == 0) throw new Exception("Impossivel dividir por zero");
+            if (y == 0) throw new Exception("Impossível dividir por zero");
             return x / y;
         }
         /* Comentário para testar o Build & Test */
